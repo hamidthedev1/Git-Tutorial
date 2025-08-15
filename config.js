6d98e6dac7ea1fdcb942d5d254ab87e88d88546d
hamidthedev1 <hamidabuhayat1>
2025-08-15 06:20:36 +0300

    version 2

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 console.log("config.js");
-console.log("version 1");
+console.log("version 2");
 // git config --global alias.s "status"
 // git config --global alias.cm "commit"
 // git config --global alias.co "checkout"
--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 console.log("hi index.js")
-console.log("version 1")
+console.log("version 2")
