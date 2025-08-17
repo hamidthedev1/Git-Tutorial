e55fa8f0fc950ed6a823f06a71e798ca51752545
hamidthedev1 <hamidabuhayat1>
2025-08-17 08:48:07 +0300

    version 12

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 console.log("config.js");
-console.log("version 11");
+console.log("version 12");
 // git config --global alias.s "status"
 // git config --global alias.cm "commit"
 // git config --global alias.co "checkout"
